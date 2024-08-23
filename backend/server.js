fa491f3506d9dd4e97e52c71f63d90e031cd0da4
New Username <new.email@example.com>
2024-08-24 00:25:12 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,10 @@ app.use(cors({
 connectDB();
 
 // Serving the frontend
-app.use(express.static(path.resolve(__dirname, 'frontend')));
+app.use(express.static(path.resolve(__dirname, 'frontend', 'dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
 // API endpoints
